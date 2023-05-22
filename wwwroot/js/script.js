69a2417335b46272ae24ba3cec1861d83c0d03f7
PosikBoy <134225302+PosikBoy@users.noreply.github.com>
2023-05-22 15:36:24 +0300

    Fix

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   $(".header__burger").click(function (event) {
     $(".header__burger, .header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
+
+$(document).ready(function () {
+  $(".menu__button").click(function (event) {
+    $(".header__burger, .header__menu").toggleClass("active");
   });
 });
 
