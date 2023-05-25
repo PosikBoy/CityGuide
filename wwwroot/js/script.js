2d48016980f608a3b156fa560967fd52339d2c4b
PosikBoy <134225302+PosikBoy@users.noreply.github.com>
2023-05-26 01:39:50 +0300

    Add files via upload

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -12,10 +12,60 @@ $(document).ready(function () {
   });
 });
 
-new Swiper(".image-slider", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
+let isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
   },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+let body = document.querySelector("body");
+if (isMobile.any()) {
+  body.classList.add("touch");
+  let arrow = document.querySelectorAll(".arrow");
+  for (i = 0; i < arrow.length; i++) {
+    let thisLink = arrow[i].previousElementSibling;
+    let subMenu = arrow[i].nextElementSibling;
+    let thisArrow = arrow[i];
+
+    thisLink.classList.add("parent");
+    arrow[i].addEventListener("click", function () {
+      subMenu.classList.toggle("open");
+      thisArrow.classList.toggle("active");
+    });
+  }
+} else {
+  body.classList.add("mouse");
+}
+
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".scroll_to_top").fadeIn();
+    } else {
+      $(".scroll_to_top").fadeOut();
+    }
+  });
+  $(".scroll_to_top").click(function () {
+    $("body,html").animate({ scrollTop: 0 }, 700);
+  });
 });
